b79863e427d6975daf8221a698f13860b5d9cd85
arshit30 <40663191+arshit30@users.noreply.github.com>
2021-03-19 09:27:45 +0530

    Delete analytics directory

--- analytics/files.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def read_data(file,ftype='csv'):
-    if ftype=='csv':
-        dataset=pd.read_csv(file)
-    elif ftype=='json':
-        dataset=pd.read_json(file)
-    return dataset
